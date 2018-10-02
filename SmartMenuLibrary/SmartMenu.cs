9ea8286e090fa5eec800b2d9679fccfb5a3be085
Johannes Ehlers Nyholm Thomsen <joha321j@edu.eal.dk>
2018-10-02 13:31:54 +0200

    Create library.

---
+++ SmartMenuLibrary/SmartMenu.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SmartMenuLibrary
+{
+    interface IBindings
+    {
+        void Call(string callId);
+    }
+    public class SmartMenu
+    {
+        //Variables
+        string menuName = string.Empty;
+        string menuDescription = string.Empty;
+        List<string> menuList = new List<string>();
+        List<string> menuId = new List<string>();
+
+        public void LoadMenu(string path)
+        {
+            int lineCounter = 0;
+            string line;
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        menuList.Add(line);
+                        if (menuList[lineCounter].Contains(";"))
+                        {
+                            string[] tempStringArray = menuList[lineCounter].Split(';');
+                            menuList[lineCounter] = tempStringArray[0];
+                            menuId.Add(tempStringArray[1]);
+                        }
+                        lineCounter++;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read.");
+                Console.WriteLine(e.Message);
+            }
+
+
+
+            //Moving the first 2 items in menuList to ensure that menuId and menuList should have same dimensions.
+            menuName = menuList[0];
+            menuDescription = menuList[1];
+            menuList.RemoveAt(0);
+            menuList.RemoveAt(0);
+        }
+        public void Activate()
+        {
+            int userInput = 1;
+
+            while (userInput != 0)
+            {
+                //Print the loaded menu
+                Console.Clear();
+                Console.WriteLine(menuName);
+                Console.WriteLine(menuDescription);
+                for (int lineCount = 0; lineCount < menuList.Count; lineCount++)
+                {
+                    Console.WriteLine("  {0}.  {1}", lineCount + 1, menuList[lineCount]);
+                }
+
+                Console.WriteLine("Tast linie nummeret for at tilgå den undermenu.");
+                Console.WriteLine("Tast 0 for at afslutte.");
+
+
+                //Ensure input is a valid number.
+                while (!int.TryParse(Console.ReadLine(), out userInput) || userInput > menuId.Count)
+                {
+                    Console.WriteLine("Indtast venligst et gyldigt tal.");
+                }
+                if (userInput > 0)
+                {
+
+                    Bindings.Call(menuId[userInput - 1]);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d49815b4-88eb-47e7-8db6-009080f051d4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
